36ad217a7e34f838f13a686a57593a523990b527
Akhil Marayikkottu Vijayan <akhilmarayikkottuvijayan@Akhils-MacBook-Air-2.local>
2024-08-25 19:04:33 -0400

    Bayesian Linear Regression: Final

--- Linear_Models/Regression/Bayesian Linear Regression.py
+++ Linear_Models/Regression/Bayesian Linear Regression.py
@@ -27,22 +27,16 @@
 #   
 #   $$P (\vec{w}) = \mathcal{N}(w_0|0,\alpha).\mathcal{N}(w_1|0,\alpha)$$
 
-import FUN 
+#import FUN 
 import random
 import numpy as np
 from scipy import *
 import matplotlib.pyplot as plt
-
-
-def JointNormalSamp(x1,x2,mu1,mu2,Sigma):
-    import numpy as np
-    Det_sqrt = np.sqrt(np.linalg.det(Sigma))
-    temp1    =  1/np.sqrt(2*np.pi)
-    temp2    = np.array([[x1-mu1],[x2-mu2]])
-    temp3    = np.dot(temp2.T,Sigma)
-    temp4    = np.dot(temp3,temp2)
-    pdf      =(temp1*np.exp(-0.5*temp4)/Det_sqrt)
-    return(pdf)  
+from scipy.stats import multivariate_normal
+from scipy.stats import norm
+import functions
+import matplotlib as mpl
+from matplotlib import  ticker
 
 
 def model(w0,w1,x):
@@ -50,143 +44,250 @@ def model(w0,w1,x):
     return(y)
 
 
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif"
+})
+
 # +
+x, y = np.mgrid[-1:1:.001, -1:1:.001]
+pos = np.dstack((x, y))
+
+xsam = np.linspace(-1,1,10)
+
+beta       = 0.2
+alpha      = 0.2
+Sigma_data = beta*np.array([[1,0],[0,1]])
+# -
+
+# ## Initially when we have only the prior distribution
+
+# +
+prior_mean = [0.0,0.0]
+prior_covariance = [[alpha, 0], [0, alpha]]
+prior = multivariate_normal(prior_mean, prior_covariance )
+
+fig,axs = plt.subplots(1,3,sharey=False,figsize=(22,6))
+axs[0].set_xlabel('$w_0$',fontsize=22); axs[0].set_ylabel('$w_1$',fontsize=22)
+axs[1].set_xlabel('$w_0$',fontsize=22); axs[1].set_ylabel('$w_1$',fontsize=22)
+axs[2].set_xlabel('$x$',fontsize=22); axs[2].set_ylabel('$y$',fontsize=22)
+axs[0].set_xlim(-1,1);axs[0].set_ylim(-1,1)
+axs[1].set_xlim(-1,1);axs[1].set_ylim(-1,1)
+axs[2].set_xlim(-1,1);axs[2].set_ylim(-1,1)
+
+axs[1].contourf(x, y, prior.pdf(pos),cmap='jet',levels=100)
+
+for i in range (0,100):
+    w0 = random.uniform(-1,1)
+    w1 = random.uniform(-1,1)
+    W  = [w0,w1]
+    prob = prior.pdf(W)
+    if(prob > random.random()):
+        axs[2].plot(xsam,model(w0,w1,xsam),'b',linewidth=0.5)
+
+axs[2].plot(xsam,model(-0.3,0.5,xsam),'r',linewidth=2)
+axs[0].tick_params(axis='both', which='major', labelsize=18)
+axs[1].tick_params(axis='both', which='major', labelsize=18)
+axs[2].tick_params(axis='both', which='major', labelsize=18)
+
+axs[0].set_title('likelihood',fontsize=24)
+axs[1].set_title('prior/posterior',fontsize=24)
+axs[2].set_title('data space',fontsize=24)
+
+axs[0].plot(-0.3,0.5, 'wx', markersize=22)
+axs[1].plot(-0.3,0.5, 'wx', markersize=22)
+
+plt.savefig('Images/Stage0.png')
+# -
+
+# ### Get an observed point varying with a Gaussian of std dev 0.2
+
+# +
+x_sam1 = random.uniform(-1,1)
+y_obs1 = model(-0.3,0.5,x_sam1)+random.gauss(0,beta)
+
 w0 = np.linspace(-1,1,100)
 w1 = w0
-mu0 = 0
-mu1 = 0
 
-Sigma = np.array([[0.2,0],[0,0.2]])
-PRIOR = FUN.JointNormal(w0,w1,mu0,mu1,Sigma)
-
-# +
-fig,axs = plt.subplots(1,2,sharey=False,figsize=(9,4))
-axs[0].contourf(w0,w1,PRIOR,cmap='jet',levels=250)
-axs[0].set_xlabel('$w_0$'); axs[0].set_ylabel('$w_1$')
-
-Nsam = 10
-xsam = np.linspace(-1,1,10)
-for i in range(0,Nsam):
-    w0sam = -1+2*random.random()
-    w1sam = -1+2*random.random()
-    Prob  =JointNormalSamp(w0sam,w1sam,mu0,mu1,Sigma)
-    # acceprance rejection
-    if(Prob > random.random()):
-        axs[1].plot(xsam,model(w0sam,w1sam,xsam),'k')
-
-axs[1].plot(xsam,model(-0.3,0.5,xsam),'r',linewidth=4)
-axs[1].set_xlabel('$x$');axs[1].set_ylabel('$y$')    
-axs[1].set_xlim(-1,1);axs[1].set_ylim(-1,1)
-
-# -
-
-# Figure one shows the prior distribution $P(w_0, w_1)$ and figure two shows the realization of the model $y = w_0+w_1.x$ for randomly picked parameter points $\vec{w}^*$ from the prior distribution. Note that the 
-
-# Assume that the true data points are from a line $t = -0.3+0.5x$ with a noise level of $\beta = 25$. For example, at $x = 0.5$, the true value is -0.05. Adding a bit of noise to it $\sim \mathcal{N}(0,0.02)$. The new point with the noise can be something like 
-#
-# $$\tilde{t} = -0.05 + \mathcal{N}(0, 0.02) \sim -0.0343$$
-#
-# The distribution of the likelihood function which shows the distribution of the predicted target $\tilde{t}$ for a $\tilde{x}$, $P(\tilde{t}|\vec{w})$ is given as:
-#
-# $$P(\tilde{t}| \tilde{x}, \vec{w})   = \mathcal{N}\bigg(\tilde{t}\bigg| ~\underbrace{w_0+w_1 \tilde{x}}_\text{model}~~, \beta \bigg) $$
-#
-# $$P(\tilde{t}|\tilde{x}, \vec{w})   \propto \exp \Bigg( \frac{[\tilde{t} - (w_0 + w_1. \tilde{x})]^T.[\tilde{t} - (w_0 + w_1. \tilde{x})]}{\beta} \Bigg) $$
-
-# +
-#-0.05+np.random.normal(0,0.02)
-np.size(w0)
-beta       = .2
-Sigma_data = beta*np.array([[1,0],[0,1]])
-
-t_tilde = -0.05+np.random.normal(0,beta)
-x_tilde =  0.5
-Likelihood = np.zeros((np.size(w0),np.size(w1)))
+likelihood1 = np.zeros((np.size(w0),np.size(w1)))
 for i in range(0,np.size(w0)):
     for j in range (0,np.size(w1)):
         Det_sqrt = np.sqrt(np.linalg.det(Sigma_data))
         temp1    =  1/np.sqrt(2*np.pi)
-        temp2    = -0.5*((w0[i]+w1[j]*x_tilde))**2/beta**2
-        Likelihood[i][j] = temp1*np.exp(temp2)/Det_sqrt
+        temp2    = -0.5*((w0[i]+w1[j]*x_sam1))**2/beta**2
+        likelihood1[i][j] = temp1*np.exp(temp2)/Det_sqrt
 
 # +
-fig,axs = plt.subplots(1,2,sharey=False,figsize=(9,4))
-axs[0].contourf(w0,w1,Likelihood.T,cmap='jet',levels=250)
-axs[0].set_xlabel('$w_0$'); axs[0].set_ylabel('$w_1$')
-axs[0].plot(-0.3,0.5,'wo',markersize=10,mfc='none')
+Phi = functions.BayLinReg(1,[x_sam1])
+SN_inv = alpha*np.identity(2)+beta*np.matmul(Phi.T,Phi)
 
-Nsam = 10
-xsam = np.linspace(-1,1,10)
-for i in range(0,Nsam):
-    w0sam = -1+2*random.random()
-    w1sam = -1+2*random.random()
-    Prob  =JointNormalSamp(w0sam,w1sam,mu0,mu1,Sigma)
-    # acceprance rejection
-    if(Prob > random.random()):
-        axs[1].plot(xsam,model(w0sam,w1sam,xsam),'k')
+mN = np.matmul(beta*np.linalg.inv(SN_inv),np.matmul(Phi.T, [y_obs1]))
 
-axs[1].plot(xsam,model(-0.3,0.5,xsam),'r',linewidth=4)
-axs[1].set_xlabel('$x$');axs[1].set_ylabel('$y$')    
+posterior1 = multivariate_normal(mN, np.linalg.inv(SN_inv) )
+
+# +
+fig,axs = plt.subplots(1,3,sharey=False,figsize=(22,6))
+axs[0].set_xlabel('$w_0$',fontsize=22); axs[0].set_ylabel('$w_1$',fontsize=22)
+axs[1].set_xlabel('$w_0$',fontsize=22); axs[1].set_ylabel('$w_1$',fontsize=22)
+axs[2].set_xlabel('$x$',fontsize=22); axs[2].set_ylabel('$y$',fontsize=22)
+axs[0].set_xlim(-1,1);axs[0].set_ylim(-1,1)
 axs[1].set_xlim(-1,1);axs[1].set_ylim(-1,1)
-axs[1].plot(x_tilde,t_tilde,'bo',markersize=12,mfc='none')
+axs[2].set_xlim(-1,1);axs[2].set_ylim(-1,1)
+axs[2].plot(xsam,model(-0.3,0.5,xsam),'r',linewidth=2)
+axs[0].tick_params(axis='both', which='major', labelsize=18)
+axs[1].tick_params(axis='both', which='major', labelsize=18)
+axs[2].tick_params(axis='both', which='major', labelsize=18)
+axs[2].plot(x_sam1,y_obs1,'ko',markersize=18,mfc='w')
+
+
+axs[0].contourf(w0,w1,likelihood1,cmap='jet',levels=250)
+
+axs[1].contourf(x, y, posterior1.pdf(pos),cmap='jet',levels=250)
+
+for i in range (0,100):
+    w0 = random.uniform(-1,1)
+    w1 = random.uniform(-1,1)
+    W  = [w0,w1]
+    prob = posterior1 .pdf(W)
+    if(prob > random.uniform(0,1)):
+        axs[2].plot(xsam,model(w0,w1,xsam),'b',linewidth=0.5)
+
+axs[0].set_title('likelihood',fontsize=24)
+axs[1].set_title('prior/posterior',fontsize=24)
+axs[2].set_title('data space',fontsize=24)
+
+axs[0].plot(-0.3,0.5, 'wx', markersize=22)
+axs[1].plot(-0.3,0.5, 'wx', markersize=22)
+
+plt.savefig('Images/Stage1.png')
+# -
+
+# ## Observe second datapoint 
 
 # +
-plt.figure(figsize=(4,4))
-posterior = PRIOR*Likelihood.T
+x_sam2 = random.uniform(-1,1)
+y_obs2 = model(-0.3,0.5,x_sam2)+random.gauss(0,beta)
 
-plt.contourf(w0,w1,posterior,cmap='jet',levels=250)
-plt.plot(-0.3,0.5,'wo',markersize=10,mfc='none')
-
-# +
-x_tilde2 = -.25
-t_tilde2 = model(-0.3,0.5,x_tilde2)+np.random.normal(0,beta)
-
-Likelihood2 = np.zeros((np.size(w0),np.size(w1)))
+w0 = np.linspace(-1,1,100)
+w1 = w0
+likelihood2 = np.zeros((np.size(w0),np.size(w1)))
 for i in range(0,np.size(w0)):
     for j in range (0,np.size(w1)):
         Det_sqrt = np.sqrt(np.linalg.det(Sigma_data))
         temp1    =  1/np.sqrt(2*np.pi)
-        temp2    = -0.5*(t_tilde2-model(w0[i],w1[j],x_tilde2))**2/beta**2
-        Likelihood2[i][j] = temp1*np.exp(temp2)/Det_sqrt
+        temp2    = -0.5*((w0[i]+w1[j]*x_sam2))**2/beta**2
+        likelihood2[i][j] = temp1*np.exp(temp2)/Det_sqrt
 
 # +
-fig,axs = plt.subplots(1,2,sharey=False,figsize=(9,4))
-axs[0].contourf(w0,w1,Likelihood2.T,cmap='jet',levels=250)
-axs[0].set_xlabel('$w_0$'); axs[0].set_ylabel('$w_1$')
-axs[0].plot(-0.3,0.5,'wo',markersize=10,mfc='none')
+Phi = functions.BayLinReg(2,[x_sam1,x_sam2])
+SN_inv = alpha*np.identity(2)+beta*np.matmul(Phi.T,Phi)
 
-Nsam = 10
-xsam = np.linspace(-1,1,10)
-for i in range(0,Nsam):
-    w0sam = -1+2*random.random()
-    w1sam = -1+2*random.random()
-    Prob  =JointNormalSamp(w0sam,w1sam,mu0,mu1,Sigma)
-    # acceprance rejection
-    if(Prob > random.random()):
-        axs[1].plot(xsam,model(w0sam,w1sam,xsam),'k')
+mN = np.matmul(beta*np.linalg.inv(SN_inv),np.matmul(Phi.T, [y_obs1,y_obs2]))
 
-axs[1].plot(xsam,model(-0.3,0.5,xsam),'r',linewidth=4)
-axs[1].set_xlabel('$x$');axs[1].set_ylabel('$y$')    
+posterior2 = multivariate_normal(mN, np.linalg.inv(SN_inv) )
+
+# +
+fig,axs = plt.subplots(1,3,sharey=False,figsize=(22,6))
+axs[0].set_xlabel('$w_0$',fontsize=22); axs[0].set_ylabel('$w_1$',fontsize=22)
+axs[1].set_xlabel('$w_0$',fontsize=22); axs[1].set_ylabel('$w_1$',fontsize=22)
+axs[2].set_xlabel('$x$',fontsize=22); axs[2].set_ylabel('$y$',fontsize=22)
+axs[0].set_xlim(-1,1);axs[0].set_ylim(-1,1)
 axs[1].set_xlim(-1,1);axs[1].set_ylim(-1,1)
-axs[1].plot(x_tilde,t_tilde,'bo',markersize=12,mfc='none')
-axs[1].plot(x_tilde2,t_tilde2,'bo',markersize=12,mfc='none')
-
-# +
-posterior = posterior*Likelihood2.T
-plt.figure(figsize=(4,4))
-
-
-plt.contourf(w0,w1,posterior,cmap='jet',levels=250)
-plt.plot(-0.3,0.5,'wo',markersize=10,mfc='none')
-
-# +
-AA = Likelihood.T*Likelihood2.T
-plt.figure(figsize=(4,4))
+axs[2].set_xlim(-1,1);axs[2].set_ylim(-1,1)
+axs[2].plot(xsam,model(-0.3,0.5,xsam),'r',linewidth=2)
+axs[0].tick_params(axis='both', which='major', labelsize=18)
+axs[1].tick_params(axis='both', which='major', labelsize=18)
+axs[2].tick_params(axis='both', which='major', labelsize=18)
+axs[2].plot(x_sam1,y_obs1,'ko',markersize=18,mfc='w')
+axs[2].plot(x_sam2,y_obs2,'ko',markersize=18,mfc='w')
 
 
-plt.contourf(w0,w1,AA,cmap='jet',levels=250)
-plt.plot(-0.3,0.5,'wo',markersize=10,mfc='none')
+axs[0].contourf(w0,w1,likelihood2,cmap='jet',levels=50)
+
+axs[1].contourf(x, y, posterior2.pdf(pos),cmap='jet', levels=250)
+
+for i in range (0,100):
+    w0 = random.uniform(-1,1)
+    w1 = random.uniform(-1,1)
+    W  = [w0,w1]
+    prob = posterior2 .pdf(W)
+    if(prob > random.uniform(0,1)):
+        axs[2].plot(xsam,model(w0,w1,xsam),'b',linewidth=0.5)
+
+axs[0].set_title('likelihood',fontsize=24)
+axs[1].set_title('prior/posterior',fontsize=24)
+axs[2].set_title('dataspace',fontsize=24)
+
+axs[0].plot(-0.3,0.5, 'wx', markersize=22)
+axs[1].plot(-0.3,0.5, 'wx', markersize=22)
+
+plt.savefig('Images/Stage2.png')
 # -
 
-model
+# ## Generating $N$ observed points
+
+# +
+Np = 100
+
+x_obs = np.random.uniform(-1,1,size=Np)
+
+y_obs = np.zeros(Np)
+for i in range (0,Np):
+    y_obs[i] = model(-0.3,0.5,x_obs[i])+random.gauss(0,beta)
+
+w0 = np.linspace(-1,1,100)
+w1 = w0
+likelihood3 = np.zeros((np.size(w0),np.size(w1)))
+for i in range(0,np.size(w0)):
+    for j in range (0,np.size(w1)):
+        Det_sqrt = np.sqrt(np.linalg.det(Sigma_data))
+        temp1    =  1/np.sqrt(2*np.pi)
+        temp2    = -0.5*((w0[i]+w1[j]*x_obs[-1]))**2/beta**2
+        likelihood3[i][j] = temp1*np.exp(temp2)/Det_sqrt
+
+# +
+Phi = functions.BayLinReg(Np,x_obs)
+SN_inv = alpha*np.identity(2)+beta*np.matmul(Phi.T,Phi)
+
+mN = np.matmul(beta*np.linalg.inv(SN_inv),np.matmul(Phi.T, y_obs))
+
+posterior3 = multivariate_normal(mN, np.linalg.inv(SN_inv) )
+
+# +
+fig,axs = plt.subplots(1,3,sharey=False,figsize=(22,6))
+axs[0].set_xlabel('$w_0$',fontsize=22); axs[0].set_ylabel('$w_1$',fontsize=22)
+axs[1].set_xlabel('$w_0$',fontsize=22); axs[1].set_ylabel('$w_1$',fontsize=22)
+axs[2].set_xlabel('$x$',fontsize=22); axs[2].set_ylabel('$y$',fontsize=22)
+axs[0].set_xlim(-1,1);axs[0].set_ylim(-1,1)
+axs[1].set_xlim(-1,1);axs[1].set_ylim(-1,1)
+axs[2].set_xlim(-1,1);axs[2].set_ylim(-1,1)
+axs[2].plot(xsam,model(-0.3,0.5,xsam),'r',linewidth=2)
+axs[0].tick_params(axis='both', which='major', labelsize=18)
+axs[1].tick_params(axis='both', which='major', labelsize=18)
+axs[2].tick_params(axis='both', which='major', labelsize=18)
+axs[2].plot(x_obs,y_obs,'ko',markersize=18,mfc='w')
+
+axs[0].contourf(w0,w1,likelihood3,cmap='jet',levels=50)
+
+axs[1].contourf(x, y, posterior3.pdf(pos),cmap='jet', levels=250)
+
+for i in range (0,100):
+    w0 = random.uniform(-1,1)
+    w1 = random.uniform(-1,1)
+    W  = [w0,w1]
+    prob = posterior3 .pdf(W)
+    if(prob > random.uniform(0,1)):
+        axs[2].plot(xsam,model(w0,w1,xsam),'b',linewidth=0.5)
+
+axs[0].set_title('likelihood',fontsize=24)
+axs[1].set_title('prior/posterior',fontsize=24)
+axs[2].set_title('dataspace',fontsize=24)
+
+axs[0].plot(-0.3,0.5, 'wx', markersize=22)
+axs[1].plot(-0.3,0.5, 'wx', markersize=22)
+
+
+plt.savefig('Images/Stage3.png')
+# -
 
 
