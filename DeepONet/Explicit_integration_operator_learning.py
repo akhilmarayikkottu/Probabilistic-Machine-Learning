9cc68d4a2f1aa5228c14bdbff897addf6b802cd1
Akhil Marayikkottu Vijayan <akhilmarayikkottuvijayan@Akhils-MacBook-Air-2.local>
2025-02-12 23:30:10 -0500

    Not converging : WIP

--- DeepONet/Explicit_integration_operator_learning.py
+++ DeepONet/Explicit_integration_operator_learning.py
@@ -5,6 +5,10 @@ import sys
 sys.path.append('../Python_libs')
 import NNlibs
 import random
+import tqdm
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
 plt.rcParams.update({
     "text.usetex": True,
@@ -12,11 +16,13 @@ plt.rcParams.update({
 })
 
 
-N_m = 10
-A_dict = [1,2,4]
-N_y = 10
+N_m    = 50
+A_dict = [1,2,4,6,8,10,12]
+N_y    = 10
+N_p    = 4
+epoch  = 1000
 
-#data = np.zeros((N_y*len(A_dict),N_m+2))
+criteria = nn.MSELoss()
 
 Sensor = []
 Y      = []
@@ -34,33 +40,69 @@ idx = np.round(np.linspace(0, len(x) - 1, N_m)).astype(int)
 
 plt.figure(figsize=(8,5))
 for i in  A_dict:
-	plt.plot(x,U(x,i),'b',alpha=.4)
-	plt.plot(x,G_of_U(x,i),'r',alpha=.4)
-	plt.plot(x[idx],-2*np.ones(N_m),'rx')
+#	plt.plot(x,U(x,i),'b',alpha=.4)
+#	plt.plot(x,G_of_U(x,i),'r',alpha=.4)
+#	plt.plot(x[idx],-2*np.ones(N_m),'rx')
 	for k in range(0,N_y):
 		random_idx = random.randint(0,len(x)-1)
 		y = x[random_idx]
 		g = G_of_U(x,i)[random_idx]
-		plt.plot(y,g,'ko')
+#		plt.plot(y,g,'ko')
 		Sensor.append(U(x,i)[idx])
 		Y.append(x[random_idx])
 		G.append(g)
  	
-plt.xlabel('$x$', fontsize=18)
-plt.ylabel('$U$ and $G(U)$',fontsize=16)
+#plt.xlabel('$x$', fontsize=18)
+#plt.ylabel('$U$ and $G(U)$',fontsize=16)
 plt.xlim(0,np.pi*2)
-plt.savefig('testimage.png',bbox_inches='tight')
+#plt.savefig('testimage.png',bbox_inches='tight')
 
 ### Convert numpy arrays to tensors
 
-U = torch.tensor(np.array(Sensor),dtype=torch.float32)
-Y = torch.tensor(np.array(Y).reshape(N_y*len(A_dict),1),dtype=torch.float32)
-G = torch.tensor(np.array(G).reshape(N_y*len(A_dict),1),dtype=torch.float32)
-
-print(G)
+U_train = torch.tensor(np.array(Sensor),dtype=torch.float32)
+Y_train = torch.tensor(np.array(Y).reshape(N_y*len(A_dict),1),dtype=torch.float32)
+G_train = torch.tensor(np.array(G).reshape(N_y*len(A_dict),1),dtype=torch.float32)
 
 #### Model Initialization and Training
 
-DNN = NNlibs.DeepONet(branch_input=10,trunk_input=1,latent_layer=8)
+DNN = NNlibs.DeepONet(branch_input=N_m,branch_num_layers=4,
+	branch_layer_width=8,
+	trunk_input=1,
+	trunk_num_layers=4,trunk_layer_width=8,latent_layer=N_p)
+optimizer = optim.Adam(DNN.parameters(), lr=0.001)
 
-DNN(U,Y)
+for i in range (0,epoch):
+	y_pred = DNN(U_train,Y_train)
+	y_trgt = G_train
+	loss = criteria(y_pred,y_trgt)
+	print('Epoch,  Loss: ', i,loss)
+
+	optimizer.zero_grad()
+	loss.backward()
+	optimizer.step()
+
+
+#### PREDICTION #######
+
+
+U_pred = U(x,2)[idx]
+plt.plot(x,U(x,2),'r')
+plt.plot(x,G_of_U(x,2),'b')
+plt.plot(x[idx],U(x,2)[idx],'kx')
+
+x_pred = np.linspace(0,2*np.pi,500)
+
+U_lst = []
+for i in range (0,500):
+	U_lst.append(U_pred)
+
+U_pred = torch.tensor(np.array(U_lst),dtype=torch.float32)
+Y_pred = torch.tensor(np.array(x_pred).reshape(500,1),dtype=torch.float32)
+
+G_pred = DNN(U_pred,Y_pred)
+G_pred = G_pred.detach().numpy().reshape(500)
+
+plt.plot(x_pred, G_pred, 'k')
+plt.xlabel('$x$',fontsize=18)
+plt.ylabel('$G(U)$', fontsize=18)
+plt.savefig('Prediction.png',bbox_inches='tight')
